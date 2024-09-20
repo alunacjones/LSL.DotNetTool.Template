63dd7056c8a9acd74767e79860b49963c4214d91
Alun Jones <alunacjones@gmail.com>
2024-09-20 18:32:22 +0100

    use switch expression to tidy up

--- templates/src/LSL.DotNetTool.Cli/Infrastructure/DefaultConsole.cs
+++ templates/src/LSL.DotNetTool.Cli/Infrastructure/DefaultConsole.cs
@@ -6,36 +6,22 @@ public class DefaultConsole : IConsole
 {
     private readonly ConsoleOptions _options;
 
-    public DefaultConsole(IOptions<ConsoleOptions> options)
-    {
-        _options = options.Value;
-    }
+    public DefaultConsole(IOptions<ConsoleOptions> options) => _options = options.Value;
 
     public IConsole Write(string text, bool includeNewLine, IEnumerable<object> args)
     {
-        if (includeNewLine)
+        IConsole Execute(Action action)
         {
-            if (args.Any())
-            {
-                _options.TextWriter.WriteLine(text, args.ToArray());
-            }
-            else
-            {
-                _options.TextWriter.WriteLine(text);
-            }
-        }
-        else
-        {
-            if (args.Any())
-            {
-                _options.TextWriter.Write(text, args.ToArray());
-            }
-            else
-            {
-                _options.TextWriter.Write(text);
-            }
+            action();
+            return this;
         }
 
-        return this;
+        return (includeNewLine, args) switch
+        {
+            (true, _) when args.Any() => Execute(() => _options.TextWriter.WriteLine(text, args.ToArray())),
+            (true, _) => Execute(() => Execute(() => _options.TextWriter.WriteLine(text))),
+            (false, _) when args.Any() => Execute(() => _options.TextWriter.Write(text, args.ToArray())),
+            (false, _) => Execute(() => _options.TextWriter.Write(text))
+        };
     }
 }
