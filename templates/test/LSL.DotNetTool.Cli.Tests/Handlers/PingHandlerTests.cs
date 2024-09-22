e7b16c92bb6b991b9575045772cf65efc23921ed
Alun Jones <alunacjones@gmail.com>
2024-09-22 21:45:15 +0100

    ensure triple quotes work nicely

--- templates/test/LSL.DotNetTool.Cli.Tests/Handlers/PingHandlerTests.cs
+++ templates/test/LSL.DotNetTool.Cli.Tests/Handlers/PingHandlerTests.cs
@@ -2,10 +2,28 @@ namespace LSL.DotNetTool.Cli.Tests.Handlers;
 
 public class PingHandlerTests : BaseCliTest
 {
-    [TestCase(new string[] { "ping"}, "Pong\r\n")]
-    [TestCase(new string[] { "ping", "aha" }, "aha\r\n")]
-    [TestCase(new string[] { "--verbose", "ping" }, "[INF] Executing ping\r\nPong\r\n")]
-    [TestCase(new string[] { "ping", "aha", "--verbose" }, "[INF] Executing ping\r\naha\r\n")]
+    [TestCase(new string[] { "ping"}, 
+        """
+        Pong
+
+        """)]
+    [TestCase(new string[] { "ping", "aha" }, 
+        """
+        aha
+
+        """)]
+    [TestCase(new string[] { "--verbose", "ping" }, 
+        """
+        [INF] Executing ping
+        Pong
+
+        """)]
+    [TestCase(new string[] { "ping", "aha", "--verbose" }, 
+        """
+        [INF] Executing ping
+        aha
+        
+        """)]
     public async Task Given_ValidArguments_ItShouldReturnTheExpectedResult(string[] args, string expectedOutput)
     {
         // Arrange
@@ -18,6 +36,6 @@ public class PingHandlerTests : BaseCliTest
         using var _ = new AssertionScope();
 
         result.Should().Be(0);
-        output.Should().Be(expectedOutput);
+        output.Should().Be(expectedOutput.NormaliseLineEndings());
     }
 }
