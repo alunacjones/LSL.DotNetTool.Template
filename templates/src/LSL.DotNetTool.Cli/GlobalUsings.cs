e572080354f2f486d0b6e57e233c522f1419e2ab
Alun Jones <alunacjones@gmail.com>
2024-09-22 16:58:31 +0100

    ease my life more

--- templates/src/LSL.DotNetTool.Cli/GlobalUsings.cs
+++ templates/src/LSL.DotNetTool.Cli/GlobalUsings.cs
@@ -6,3 +6,6 @@ global using LSL.DotNetTool.Cli.Options;
 #endif
 global using LSL.DotNetTool.Cli.Infrastructure;
 global using CommandLine;
+global using CommandLineParser.DependencyInjection.Interfaces;
+global using Microsoft.Extensions.Logging;
+global using LSL.AbstractConsole;
