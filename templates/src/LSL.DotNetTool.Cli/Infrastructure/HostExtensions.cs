6161f81644556af0090864216ac43933e97c5f2f
Alun Jones <alunacjones@gmail.com>
2024-09-22 16:30:18 +0100

    get a better error

--- templates/src/LSL.DotNetTool.Cli/Infrastructure/HostExtensions.cs
+++ templates/src/LSL.DotNetTool.Cli/Infrastructure/HostExtensions.cs
@@ -1,4 +1,5 @@
 using CommandLineParser.DependencyInjection.Interfaces;
+using LSL.AbstractConsole;
 using LSL.AbstractConsole.ServiceProvider;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,11 +14,21 @@ public static class HostExtensions
         var args = host.Services.GetRequiredService<IOptions<CommandLineOptions>>().Value.Arguments;
         var consoleWriter = host.Services.GetRequiredService<IOptions<ConsoleOptions>>().Value.TextWriter;
 
-        return await host
-            .Services
-            .GetRequiredService<ICommandLineParser<int>>()
-            .ParseArgumentsAsync(
-                args,
-                c => c.HelpWriter = consoleWriter);
+        var services = host.Services;
+
+        try
+        {
+            return await services
+                .GetRequiredService<ICommandLineParser<int>>()
+                .ParseArgumentsAsync(
+                    args,
+                    c => c.HelpWriter = consoleWriter);
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            // This is thrown when the cli project has not yet had Options or Handlers added yet
+            services.GetRequiredService<IConsole>().WriteLine("It looks like this project has not yet added any Options or Handlers for the CLI");
+            return 1;
+        }
     }
 }
