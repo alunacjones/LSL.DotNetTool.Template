899c654212877545d7383e94de0fb355442e9200
Alun Jones <alunacjones@gmail.com>
2025-07-20 23:38:00 +0100

    langversion12 for the tool

--- templates/src/LSL.DotNetTool.Cli/Handlers/PingHandler.cs
+++ templates/src/LSL.DotNetTool.Cli/Handlers/PingHandler.cs
@@ -1,20 +1,11 @@
 namespace LSL.DotNetTool.Cli.Handlers;
 
-public class PingHandler : IAsyncHandler<Ping>
+public class PingHandler(ILogger<PingHandler> logger, IConsole console) : IAsyncHandler<Ping>
 {
-    private readonly ILogger<PingHandler> _logger;
-    private readonly IConsole _console;
-
-    public PingHandler(ILogger<PingHandler> logger, IConsole console)
-    {
-        _logger = logger;
-        _console = console;
-    }
-
     public Task<int> ExecuteAsync(Ping options)
     {
-        _logger.LogInformation("Executing ping");
-        _console.WriteLine(options.PongValue);
+        logger.LogInformation("Executing ping");
+        console.WriteLine(options.PongValue);
         return Task.FromResult(0);
     }
 }
--- templates/src/LSL.DotNetTool.Cli/Infrastructure/AdditionalHelpTextProvider.cs
+++ templates/src/LSL.DotNetTool.Cli/Infrastructure/AdditionalHelpTextProvider.cs
@@ -3,18 +3,11 @@ namespace LSL.DotNetTool.Cli.Infrastructure;
 /// <summary>
 /// Provides additional help text showing the <c>--verbose</c>
 /// </summary>
-public class AdditionalHelpTextProvider : IExecuteParsingFailure<int>
+public class AdditionalHelpTextProvider(IConsole console) : IExecuteParsingFailure<int>
 {
-    private readonly IConsole _console;
-
-    public AdditionalHelpTextProvider(IConsole console)
-    {
-        _console = console;
-    }
-
     public int Execute(string[] args, IEnumerable<Error> errors)
     {
-        if (!errors.IsVersion()) _console.WriteLine("NOTE: a global --verbose flag can be used to provide debug logging");
+        if (!errors.IsVersion()) console.WriteLine("NOTE: a global --verbose flag can be used to provide debug logging");
         return 0;
     }
 }
